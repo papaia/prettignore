40a2d07017492052de02338856c2274020cbc4d0
Papaia <43409674+ItsPapaia@users.noreply.github.com>
2021-01-02 15:10:31 +0200

    fix(version): mark as string

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export const { version } = require('../package.json');
+export const { version }: { version: string } = require('../package.json');
 export * from './utils';
 export * from './format';
