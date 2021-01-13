1fa5f917e2c4b832a5f8858a6494f8d0caad68a4
Papaia <43409674+ItsPapaia@users.noreply.github.com>
2021-01-13 12:32:35 +0200

    chore(lint-staged): add missing prettier files

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,9 +1,9 @@
 const micromatch = require('micromatch');
 
-module.exports = (stagedFiles) => {
+module.exports = (files) => {
   const toRun = [];
-  const tsFiles = micromatch(stagedFiles, ['**/src/*.ts'], { ignore: 'dist' });
-  const forPrettier = micromatch(stagedFiles, ['**/*.{md,ts,json,js}'], { ignore: 'dist' });
+  const tsFiles = micromatch(files, ['**/src/*.ts'], { ignore: 'dist' });
+  const forPrettier = micromatch(files, ['**/*.{md,ts,json,js,yaml,yml}'], { ignore: 'dist' });
   if (tsFiles.length) {
     toRun.push('npm run build');
     toRun.push('npm test');
